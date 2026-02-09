c1bf7af468320017d45e1c308e74666a279968de
007indira <007.indira@rguktn.ac.in>
2026-02-09 15:33:01 +0530

    Add files via upload

---
+++ Task 1.py
@@ -0,0 +1,26 @@
+print("Enter first number")
+10
+print("Enter second number")
+20
+
+
+a = 10
+b = 20
+c = a + b
+print(c)
+
+a = 10
+b = 20
+c = a - b
+print(c)
+
+a = 10
+b = 20
+c = a * b
+print(c)
+
+a = 10
+b = 20
+c = a / b
+print(c)
+
